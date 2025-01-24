2ce732164dd00ba15644ad2a146fe0d8be728d6f
Tamam Krajem <tamamkarim71@gmail.com>
2025-01-24 22:15:17 +0200

    commit

---
+++ Tehtävä 3.2.py
@@ -0,0 +1,16 @@
+
+
+#
+hyttiluokka = input("Anna hyttiluokka (LUX, A, B, C): ").upper()
+
+# Käytetään if/elif/else
+if hyttiluokka == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+elif hyttiluokka == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif hyttiluokka == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+elif hyttiluokka == "C":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+else:
+    print("Virheellinen syöte. Anna oikein hyttiluokka (LUX, A, B, C).")
